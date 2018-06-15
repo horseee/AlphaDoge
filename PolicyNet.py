03fbea7e08f1c2acf9f36c59a7f884e12c8b0753
VainF <2218880241@qq.com>
2018-06-16 01:28:05 +0800

    a minimal example

--- PolicyNet.py
+++ PolicyNet.py
@@ -4,26 +4,40 @@ from utils import *
 import tensorflow as tf 
 
 class PolicyNet(object):
-    def __init__(self, ckpt_path):
+    def __init__(self, ckpt_path=None):
         self.ckpt = ckpt_path
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(graph=tf.Graph(), config=config)
+        self.initialize_graph()
 
     def initialize_graph(self):
         with self.sess.graph.as_default():
-            self.input = get_reference_input()
-            
+            self.input = get_reference_input() # placeholder
+            self.output = model_fn(self.input) # output tf tensor
+            if self.ckpt != None:
+                self.initialize_weights(self.ckpt)
+            else: self.sess.run(tf.global_variables_initializer())
+    
+    def initialize_weights(self, ckpt):
+        tf.train.Saver().restore(self.sess, ckpt)
+        
+    def run(self, inputs):
+        return self.sess.run([self.output], {self.input: inputs})
 
     def make_policy(self,curr_state, prev_state, prev_action):
         return 0, 1
 
-    
 bsize = 9
 feature_num = 1
 def get_reference_input():
     return tf.placeholder(tf.float32,
                            [None, bsize, bsize, feature_num],
-                           name='pos_tensor')
+                           name='pos_tensor')                      
+def model_fn(x):
+    return tf.layers.conv2d(x, filters=64, kernel_size=3, strides=1, padding='SAME')
 
-def model_fn()
+if __name__=='__main__':
+    pn = PolicyNet()
+    out = pn.run(np.zeros(shape=(1,9,9,1)))
+    print(out)
