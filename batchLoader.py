db3329be13ab7202014fda889810f92c97b5d539
horseee <www.maxinyin@qq.com>
2018-06-17 22:54:19 +0800

    modify batchloader

--- batchLoader.py
+++ batchLoader.py
@@ -41,10 +41,20 @@ class BatchLoader(object):
 			self.sgf_list[i].next()
 		return np.array(X), y 
 
-if __name__=='__main__':
-	batch = BatchLoader(dir='train')
-	for i in range(20):
-		bX, by = batch.get_batch()
-		print(bX)
-		print(by)
+	def reset_batch(self):
+		self.sgf_list = []
+		self.file_idx = 0
+
+	def batch_end(self):
+		if self.file_idx + self.batch_size >= len(self.filenames):
+			return True
+		else:
+			return False
+
+#if __name__=='__main__':
+#	batch = BatchLoader(dir='train')
+#	for i in range(20):
+#		bX, by = batch.get_batch()
+#		print(bX)
+#		print(by)
 	
