8f217848c696dba23c4463ed3074c7fdabc47c90
VainF <2218880241@qq.com>
2018-06-17 21:23:15 +0800

    add value and policy

---
+++ model.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+
+def policy_value_net(inputs):
+
+    kernel_size = [3, 3]
+    filters = 32
+    strides = 1
+    dropout_rate = 0.2
+    res_layer = 13
+
+    def ResBlock(x, filters, kernel_size, strides, dropout_rate):
+        short_cut = x
+
+        x = tf.layers.batch_normalization(x)
+        x = tf.nn.relu(x)
+        x = tf.layers.dropout(x, rate=dropout_rate)
+        x = tf.layers.conv2d(inputs=x, filters=filters, kernel_size=kernel_size, padding='SAME', strides=strides)
+
+        x = tf.layers.batch_normalization(x)
+        x = tf.nn.relu(x)
+        x = tf.layers.dropout(x, rate=dropout_rate)
+        x = tf.layers.conv2d(inputs=x, filters=filters, kernel_size=kernel_size, padding='SAME', strides=strides)
+
+        x = x + short_cut
+        print(x.shape)
+        return x
+    with tf.variable_scope("share"):
+        x = tf.layers.batch_normalization(inputs)
+        print(x.shape)
+        x = tf.layers.conv2d(inputs=x, filters=filters, kernel_size=kernel_size, padding='SAME', strides=strides)
+        for i in range(res_layer):
+            x = ResBlock(x, filters, kernel_size, strides, dropout_rate)
+
+    with tf.variable_scope("policy"):
+        policy = tf.layers.conv2d(inputs=x, filters=2, kernel_size=1, padding='SAME', strides=1)
+        policy = tf.layers.batch_normalization(policy)
+        policy = tf.nn.relu(policy)
+        policy = tf.layers.flatten(policy)
+        policy_logits = tf.layers.dense(policy, units=9*9+1) # the last one is PASS action
+        print("Policy: %s"%policy_logits.shape)
+
+    with tf.variable_scope("value"):
+        value = tf.layers.conv2d(inputs=x, filters=1, kernel_size=1, padding='SAME', strides=1)
+        value = tf.layers.batch_normalization(value)
+        value = tf.nn.relu(value)
+        value = tf.layers.flatten(value)
+        value = tf.layers.dense(value, units=64)
+        value = tf.nn.relu(value)
+        value = tf.layers.dense(value, units=1)
+        value_out = tf.nn.tanh(value)
+        print("Value: %s"%value_out.shape)
+        
+
+    return {'policy': policy_logits, 'value': value_out}
+   
+
+
+
+
+
+
+
+
+
+
+
--- trainSL.py
+++ trainSL.py
@@ -1,6 +1,6 @@
 import argparse
 import tensorflow as tf
-import resnet
+from model import policy_value_net
 
 
 if __name__ == '__main__':
@@ -13,12 +13,16 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	data_input = tf.placeholder(dtype='float32',shape=(None, 9, 9, 1))
-	label_input = tf.placeholder(dtype='float', shape=(None, 9, 9, 1))
+	label_input = tf.placeholder(dtype='float', shape=(None, 9*9+1)) #考虑PASS
 	
 	# load model
-	logits = resnet.ResNet(data_input)
-	loss = tf.losses.softmax_cross_entropy(label_input, logits)
-	opt = tf.train.AdamOptimizer(args.learning_rate).minimize(loss)
+	net_out = policy_value_net(data_input)
+
+	policy_logits = net_out['policy']
+	value = net_out['value']
+
+	policy_loss = tf.losses.softmax_cross_entropy(label_input, policy_logits)
+	opt = tf.train.AdamOptimizer(args.learning_rate).minimize(policy_loss)
 
 	# deployment and save checkpoint
 	tf_config = tf.ConfigProto()
@@ -31,24 +35,20 @@ if __name__ == '__main__':
 	sess.run(tf.global_variables_initializer())
 	saver =  tf.train.Saver(tf.global_variables())
 
-	try:
-		if os.path.exists('checkpoints'):
-			saver.restore(sess, args.ckpt)
-			print('Model restored from %s'%(args.ckpt))
-		else: 
-			print('Restore failed! Model exists but cannot reload')
-	except: 
-		print('No model restore! Training new model!')
+	if os.path.exists('checkpoints'):
+		saver.restore(sess, args.ckpt)
+		print('[!] Model restored from %s'%(args.ckpt))
+	else: 
+		print('[!] No checkpoints!')
 
 
-	for ep in range(epoch):
+
+	#for ep in range(epoch):
+
 		# total_loss = []
 		# batch_input = SGF_file.SGF_file_reader(batch_size)
 		# _, cur_loss = sess.run([opt, loss], {data_input: batch_input, label_input: batch_labels})
         # total_loss.append(cur_loss)
 
-    print('[*] epoch %d, average loss = %f'%(ep, np.mean(total_loss)))
-    saver.save(sess, args.ckpt)
-
-
-
+    #print('[*] epoch %d, average loss = %f'%(ep, np.mean(total_loss))
+    #saver.save(sess, args.ckpt)
